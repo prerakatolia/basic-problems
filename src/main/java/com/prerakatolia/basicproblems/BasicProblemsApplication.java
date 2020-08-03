dabb503a5315c6cbcad44cfa3427692a8b4148e9
Prerak Atolia <prerak.atolia@blueyonder.com>
2020-08-03 13:02:51 +0530

    Basic Setup

---
+++ src/main/java/com/prerakatolia/basicproblems/BasicProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.prerakatolia.basicproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicProblemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prerakatolia/basicproblems/BasicProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prerakatolia.basicproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
